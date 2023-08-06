e070677182d53dccc3c513a5ed482589c1f238dc
rubeennsp <30862029+rubeennsp@users.noreply.github.com>
2023-08-05 23:03:40 -0400

    Improve cauldron animation and model

---
+++ Assets/Scripts/Cauldron/CookingCauldron.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookingCauldron : MonoBehaviour
+{
+    public bool isCooking;
+
+    private Animator anim;
+
+    public void startCooking()
+    {
+        isCooking = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isCooking = Input.GetKey(KeyCode.F);
+        anim.SetBool("IsCooking", isCooking);
+    }
+}
